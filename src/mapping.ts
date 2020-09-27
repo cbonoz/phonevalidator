e57858e6145e7c18ee58a91e65d9aa737989204d
Chris Buonocore <chrisdistrict@gmail.com>
2020-09-27 12:09:22 -0400

    update log

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,6 @@
 import { LastPhoneValidated } from "../generated/PhoneValidator/PhoneValidator";
 import { PhoneValidated } from "../generated/schema";
+import { log } from "@graphprotocol/graph-ts";
 
 export function handleLastPhoneValidated(event: LastPhoneValidated): void {
   let id = event.params.requestId.toHex();
@@ -10,4 +11,10 @@ export function handleLastPhoneValidated(event: LastPhoneValidated): void {
   phone.valid = event.params.valid || true;
   phone.location = event.params.location.toString();
   phone.save();
+  log.debug("Phone validation event: {} {} {}, valid: {}", [
+    id,
+    phone.phone,
+    phone.timestamp.toString(),
+    phone.valid ? "true" : "false",
+  ]);
 }
